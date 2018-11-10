f21cf2bdbf9ddd96dd8030abc3968ba57227f181
misraelson <misraelson@gmail.com>
2018-11-10 15:41:19 -0800

    turned List into a class component, made onClick a callback so I can pass itemName to showSubitem function

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -2,35 +2,43 @@ import React, { Component } from 'react';
 
 import styles from './List.scss';
 
+export default class List extends Component {
 
-
-export default function List(props) {
-
-  let projectNames = props.items.map( (name, id) => {
-    // console.log(name.name)
-    return <h1 key={id}  onClick={showSubitem}> {name.name} </h1>;
-  })
-
-  function showSubitem() {
-    props.items.map( (sites, id) => {
-      console.log(sites.sites)
-      sites.sites.map( (site) => {
-        console.log(site)
-        return <h3 key={id}> {site} </h3>
-      })
-    })
+  state = {
+    isOpen: false,
   }
 
-  return (
-    <div>
+  // projectNames = () => {
+  //   this.props.items.map( (item, id) => {
+  //     console.log(item.name)
+  //     return <h1 key={id}  onClick={this.showSubitem(item.name)}> {item.name} </h1>;
+  //   })
+  // }
 
-      {projectNames}
+  showSubitem = (name) => {
+    // console.log(name)
+    this.props.items.map( (item) => {
+      if(item.name === name) {
+        item.sites.map( (site, id) => {
+          console.log("this is site:", site)
+          return<h3 key={id}> {site} </h3>
+        })
+      }
+      // console.log(sites.sites)
+    })
+  }
+  render() {
 
-      {/* <h1>{props.items[0].name}</h1>
-        <h2>{props.items[0].sites[0]}</h2>
-        <h2>{props.items[0].sites[1]}</h2>
-      <h1>{props.items[1].name}</h1>
-      <h1>{props.items[2].name}</h1> */}
-    </div>
-  );
+      // console.log(item.name)
+    return (
+      <div>
+        {this.props.items.map( (item, id) => {
+          // console.log(item.name);
+          return (
+            <h1 key={id} onClick={()=> this.showSubitem(item.name)}>{item.name}</h1>
+          );
+        })};
+      </div>
+    )
+  }
 }
