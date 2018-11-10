0175d2220642ffeb01a17a3a3954bc7c27d92c47
misraelson <misraelson@gmail.com>
2018-11-10 12:23:12 -0800

    created variable projectNames that maps through props.items inside List and returns an <h1> of the project names => then called inside the return to display project names

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 
 import styles from './List.scss';
 
+
+
 export default function List(props) {
+
+  let projectNames = props.items.map( (name, id) => {
+    console.log(name.name)
+    return <h1 key={id}>{name.name}</h1>
+  })
+  
   return (
     <div>
-      <h1>THIS THING</h1>
-      <h1>{props.items[0].name}</h1>
+
+      {projectNames}
+
+      {/* <h1>{props.items[0].name}</h1>
+        <h2>{props.items[0].sites[0]}</h2>
+        <h2>{props.items[0].sites[1]}</h2>
+      <h1>{props.items[1].name}</h1>
+      <h1>{props.items[2].name}</h1> */}
     </div>
-  );
+);
 }
