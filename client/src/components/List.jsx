b8e32994f4b29d1fb8511a6e4cdfdaae781c5696
misraelson <misraelson@gmail.com>
2018-11-12 18:31:52 -0800

    successfully integrated and implemented the onClickSubitem prop, map now centers on site

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -18,18 +18,22 @@ export default class List extends Component {
       }
     })
   }
+
+  centerMap = (site) => {
+    this.props.onClickSubitem(site);
+  }
+
   render() {
     return (
       <div>
         {this.props.items.map( (item, id) => {
           // when mapping through an array inside an array must wrap in another div https://stackoverflow.com/questions/47402365/how-to-have-nested-loops-with-map-in-jsx
           return (
-            // https://stackoverflow.com/questions/47497090/react-how-to-pass-props-from-onclick-to-function?rq=1
-            <div>
-              <h1 key={id} onClick={()=> this.showSubitem(item.name)}>{item.name}</h1>
+            <div key={id}>
+              <h1 onClick={ ()=> this.showSubitem(item.name) }>{item.name}</h1>
                 {item.sites.map( (site, siteId ) => {
                   {if(this.state.openSections[item.name] === true) {
-                    return(<h3 key={siteId}> {site} </h3>)
+                    return(<h3 key={siteId} onClick={ ()=> this.centerMap(site) }> {site} </h3>)
                   }}
                 })}
             </div>
--- client/src/containers/Sidebar.jsx
+++ client/src/containers/Sidebar.jsx
@@ -9,28 +9,6 @@ import List from '../components/List';
 
 class Sidebar extends Component {
   render() {
-    /*
-      Data should be an array of items. Each item also has items that represent the sub menu. Use the data from the redux store being passed in as props.
-
-      The structure of the data is:
-
-      [
-        {
-          id: 1,
-          name: "Cypress Provincial Park",
-          items: [
-            {
-              id: 1,
-              name: 'East'
-            },
-            {
-              id: 2,
-              name: 'West'
-            }
-          ]
-        }
-      ]
-    */
     // the props in Sidebar comes from Redux and contains the Sites/Projects object-array
     const items = [...this.props.projects];
 
