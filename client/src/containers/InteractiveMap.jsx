0af8b3def6e5bd2e7a58a0af916a7ee553cbef45
misraelson <misraelson@gmail.com>
2018-11-15 16:44:08 -0800

    progress by process of elimination. I had made the wrong assumption about the source geoJSON data

--- client/src/containers/InteractiveMap.jsx
+++ client/src/containers/InteractiveMap.jsx
@@ -11,20 +11,18 @@ class InteractiveMap extends Component {
   render() {
     const { bounding } = this.props.currentSite;
     const { trees } = this.props.currentSite
+    const allTrees = {...this.props.trees};
+    // let { height, lat, long, type } = allTrees
+    let coordinatesArray = []
 
     let treeMap = () => {
       trees.map( (tree) => {
         let treeObj = {...allTrees[tree]}
-        console.log(treeObj.lat);
-        return treeObj.lat
+        // console.log([treeObj.long, treeObj.lat]);
+        coordinatesArray.push([treeObj.long, treeObj.lat])
+        // console.log(coordinatesArray)
       })
     }
-    // console.log(trees)
-
-    const allTrees = {...this.props.trees};
-    let { height, lat, long, type } = allTrees
-    // console.log({...allTrees[1975].lat});
-
 
     const boundingFeature = turf.polygon([[
       [bounding.left, bounding.top],
@@ -35,57 +33,72 @@ class InteractiveMap extends Component {
     ]], { name: 'Bounding Area' });
 
     let coordinates = [-123.18727286009813, 49.38710261999775]
-    let radius =  2;
-    let options = {name: 'Circle'};
+    let coordinates2 = [-123.18463152343836, 49.38604381672198]
+    // let options = {name: 'Circle'};
 
-    let treeFeature = turf.point(
-      coordinates, options );
+    let treeFeature = turf.point( coordinates );
+    let treeFeature2 = turf.point( coordinates2 );
 
+    let treeMaker = () => {
+      coordinatesArray.forEach( (latlong, index) => {
+        // console.log([parseFloat(latlong[0]), parseFloat(latlong[1])])
+        let newCoordinates = [parseFloat(latlong[0]), parseFloat(latlong[1])]
+        let options = {name: 'Circle'};
+        let treeCircle = turf.point(newCoordinates, options);
+        // console.log(treeCircle)
+        let geoJSON =  <GeoJSON id="tree-objects" data={ treeCircle } />
+
+        console.log(geoJSON.props.id)
+
+
+        // return geoJSON.props.id
+        return <Layer key={index} id="treeCircle" type="circle" paint={{'circle-radius': 5, 'circle-color': 'white', 'circle-stroke-color': 'black', 'circle-opacity': 0.8,}} source={geoJSON.props.id} />
+      })
+    };
+    console.log(treeMaker());
     treeMap();
 
-    // let treeMap = this.props.trees.map( tree => {
-
-      return (
-        <Map { ...this.props }>
+    return (
+      <Map { ...this.props }>
           <Sources>
             <GeoJSON id="bounding-box" data={ boundingFeature } />
-            <GeoJSON id="tree-objects" data={ treeFeature } />
-            {/* <GeoJSON id="tree-objects" data={ [tree.longitude, tree.latitude] } /> */}
+            <GeoJSON id="tree-object" data={ treeFeature } />
+            <GeoJSON id="tree-object" data={ treeFeature2 } />
           </Sources>
-          <Layer
-            id="treeCircle"
-            type="circle"
-            paint={{
-              'circle-radius': 5,
-              'circle-color': 'white',
-              'circle-stroke-color': 'black',
-              'circle-opacity': 0.8,
-            }}
-            source="tree-objects"
-          />
-          <Layer
-            id="bounding-box"
-            type="line"
-            paint={{
-              'line-width': 2,
-              'line-color': '#fff',
-              'line-opacity': 1,
-            }}
-            source="bounding-box"
-          />
-          <Layer
-            id="bounding-fill"
-            type="fill"
-            paint={{
-              'fill-color': 'grey',
-              'fill-opacity': 0.5,
-            }}
-            source="bounding-box"
-          />
+        <Layer
+          id="treeCircle"
+          type="circle"
+          paint={{
+            'circle-radius': 5,
+            'circle-color': 'white',
+            'circle-stroke-color': 'black',
+            'circle-opacity': 0.8,
+          }}
+          source="tree-object"
+        />
+        { treeMaker() }
+        <Layer
+          id="bounding-box"
+          type="line"
+          paint={{
+            'line-width': 2,
+            'line-color': '#fff',
+            'line-opacity': 1,
+          }}
+          source="bounding-box"
+        />
+        <Layer
+          id="bounding-fill"
+          type="fill"
+          paint={{
+            'fill-color': 'grey',
+            'fill-opacity': 0.5,
+          }}
+          source="bounding-box"
+        />
 
-        </Map>
-      );
-    // })
+      </Map>
+    );
   }
 }
 
